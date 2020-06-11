e70c1a9940fe75af3461803000d80823c94b2e45
Matt Bailey <matt@matt-bailey.co.uk>
2020-06-11 21:23:25 +0100

    - Add compass for wind direction - Use decoded endpoint to avoid parsing METAR - Remove TAF for now - Segment and display info in a legible way

--- js/checkwx.js
+++ js/checkwx.js
@@ -1,31 +1,18 @@
 var apiKey = "your-api-key";
 var icaoCode = "EGSH";
 
-var metarXhr = new XMLHttpRequest();
-var tafXhr = new XMLHttpRequest();
+var decodedMetarXhr = new XMLHttpRequest();
 
-metarXhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-        metarSuccess(JSON.parse(this.responseText));
-    }
-});
-
-tafXhr.addEventListener("readystatechange", function() {
+decodedMetarXhr.addEventListener("readystatechange", function () {
     if(this.readyState === 4) {
-        tafSuccess(JSON.parse(this.responseText));
+        decodedMetarSuccess(JSON.parse(this.responseText));
     }
-});
+})
 
-function getMetar() {
-    metarXhr.open("GET", "https://api.checkwx.com/metar/" + icaoCode);
-    metarXhr.setRequestHeader("X-API-KEY", apiKey);
-    metarXhr.send();
-}
-
-function getTaf() {
-    tafXhr.open('GET', "https://api.checkwx.com/taf/" + icaoCode);
-    tafXhr.setRequestHeader("X-API-KEY", apiKey);
-    tafXhr.send();
+function getDecodedMetar() {
+    decodedMetarXhr.open("GET", "https://api.checkwx.com/metar/" + icaoCode + "/decoded");
+    decodedMetarXhr.setRequestHeader("X-API-KEY", apiKey);
+    decodedMetarXhr.send();
 }
 
 function metarSuccess(response) {
@@ -37,14 +24,37 @@ function metarSuccess(response) {
     }
 }
 
-function tafSuccess(response) {
+function decodedMetarSuccess(response) {
     if (response.results > 0) {
-        var metar = response.data[0];
-        document.getElementById('taf').innerText = metar;
-    } else {
-        document.getElementById('taf').innerText = 'No results returned from API';
+        document.getElementById('station').innerText = icaoCode;
+        document.getElementById('stationName').innerText = response.data[0].station.name
+        var windDirection = response.data[0].wind.degrees;
+        var windSpeed = response.data[0].wind.speed_kts;
+        var flightCategory = response.data[0].flight_category;
+        var cloudBase = response.data[0].ceiling.feet
+        var cloudType = response.data[0].ceiling.text.toUpperCase()
+        document.getElementById('flightCategory').innerText = flightCategory;
+        switch (flightCategory) {
+            case "VFR":
+                document.getElementById('flightCategory').className = 'alert alert-success';
+                break;
+            case "LIFR":
+                document.getElementById('flightCategory').className = 'alert alert-warning';
+                break;
+            case "IFR":
+                document.getElementById('flightCategory').className = 'alert alert-danger';
+                break;
+            case "MVFR":
+                document.getElementById('flightCategory').className = 'alert alert-warning';
+                break;
+            default:
+                break;
+        }
+        document.getElementById('windSpeed').innerText = windSpeed + " KTS"
+        document.getElementById('windDirection').childNodes[0].nodeValue = windDirection + "°";
+        document.getElementById('clouds').innerText = cloudType + " @ " + cloudBase + "ft";
+        document.getElementById('arrow').style.transform = "rotate(" + windDirection + "deg)"
     }
 }
 
-setInterval(getMetar(), 90000);
-setInterval(getTaf(), 90000);
+setInterval(getDecodedMetar(), 90000);
